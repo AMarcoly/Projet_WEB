b139f9d27503e84f982d5ba9bc12fe07da346e44
Marcoly <marcoly.antoine@etu.unistra.fr>
2023-03-07 10:48:04 +0100

    Organisation des parties du code

---
+++ Projet/includes/footer.php
@@ -0,0 +1,13 @@
+</main>
+
+    <footer id="footer">
+        <p>Droits d'auteur © 2023 Marcoly ANTOINE Mon Portfolio</p>
+    </footer>
+    <nav id="menu">
+        <ul>
+            <li><a href="contact.php">Contact</a><li>
+            <li><a href="contact.php">Contact</a><li>
+            <li><a href="contact.php">Contact</a><li>
+
+        </ul>
+    </nav>
---
+++ Projet/index.php
@@ -0,0 +1,13 @@
+<?php
+    $pageID = 'index';
+    $headTitle = 'Marcoly ANTOINE- Mon portfolio';
+    $pageTitle = 'Mon <em>Portfolio</em>';
+
+    include 'includes/head.php';
+    include 'includes/header/headerFront.php';
+    include 'includes/content/frontpage/a_propos.php';
+    include 'includes/content/frontpage/projets.php';
+    include 'includes/content/frontpage/skills.php';
+    include 'includes/footer.php';
+
+?>
