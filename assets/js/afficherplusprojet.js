0b088cc86a29c607a0fd3a7d2e128a0acde09d54
ANTOINE MARCOLY <marcoly.antoine@etu.unistra.fr>
2023-04-23 23:31:13 +0200

    Suppression commentaires inutiles

--- assets/js/afficherplusprojet.js
+++ assets/js/afficherplusprojet.js
@@ -1,4 +1,3 @@
-let i=0;
 const sec = document.querySelector('conteneur_projet')
 const btn = document.querySelector('voir-plus')
 
@@ -6,12 +5,9 @@ async function nextprojets(){
     console.log("J'ai clique");
     try{
         console.log("J'ai clique")
-        const response = await fetch('assets/php/recuperer_projet.php'+i)
+        const response = await fetch('assets/php/recuperer_projet.php')
         const data = await response.text()
         console.log(data)
-
-        i++;
-        console.log(i)
         sec.innerHTML+=data;
         console.log('apres fetch')
     }catch (error){
